b8d540021c4bae3b3a6d275e61c2ea4ca5052272
Joseph Morrison <josephmorrison@postcodelottery.co.uk>
2023-01-23 21:43:37 +0000

    add readme

--- infrastructure/bin/sitestacks.ts
+++ infrastructure/bin/sitestacks.ts
@@ -21,7 +21,4 @@ new SitePreviewStack(app, stackName, {
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
-new DeploymentStack(app, "ProductionWebsite", {env: { 
-  account: process.env.CDK_DEFAULT_ACCOUNT, 
-  region: process.env.CDK_DEFAULT_REGION 
-}})
+new DeploymentStack(app, "ProductionWebsite", {})
